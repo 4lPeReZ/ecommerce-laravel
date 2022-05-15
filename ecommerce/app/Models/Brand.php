ad8222ec3f7edab9531c98f349d49710f1271b37
AlvaroPerezRey <aperez.alumno@iesrafaelalberti.es>
2022-05-15 20:21:56 +0200

    Insertar registros en la tabla Brands

--- ecommerce/app/Models/Brand.php
+++ ecommerce/app/Models/Brand.php
@@ -10,4 +10,14 @@ class Brand extends Model
     use HasFactory;
 
     protected $fillable = ['name'];
+
+    //Relacion 1 : N
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
+
+    //Relacion N : M
+    public function categories(){
+        return $this->belongsToMany(Category::class);
+    }
 }
--- ecommerce/database/factories/BrandFactory.php
+++ ecommerce/database/factories/BrandFactory.php
@@ -2,19 +2,18 @@
 
 namespace Database\Factories;
 
+use App\Models\Brand;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class BrandFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
+
+    protected $model = Brand::class;
+
     public function definition()
     {
         return [
-            //
+            'name' => $this->faker->word()
         ];
     }
 }
