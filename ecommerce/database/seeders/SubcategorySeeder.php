a78df71a04e404bb1555966d66e622f52e407045
AlvaroPerezRey <aperez.alumno@iesrafaelalberti.es>
2022-05-15 19:39:22 +0200

    Insertar registros en la tabla Subcategorias

--- ecommerce/database/seeders/SubcategorySeeder.php
+++ ecommerce/database/seeders/SubcategorySeeder.php
@@ -2,17 +2,76 @@
 
 namespace Database\Seeders;
 
+use App\Models\Subcategory;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class SubcategorySeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        $subcategories = [
+            /* Albums */
+            [
+                'category_id' => 1,
+                'name' => 'Grupo Masculino',
+                'slug' => Str::slug('Grupo Masculino')
+            ],
+
+            [
+                'category_id' => 1,
+                'name' => 'Grupo Femenino',
+                'slug' => Str::slug('Grupo Femenino')
+            ],
+
+            [
+                'category_id' => 1,
+                'name' => 'Solistas',
+                'slug' => Str::slug('Solistas')
+            ],
+
+            /* DVD-BluRay */
+            [
+                'category_id' => 2,
+                'name' => 'Grupo Masculino',
+                'slug' => Str::slug('Grupo Masculino')
+            ],
+
+            [
+                'category_id' => 2,
+                'name' => 'Grupo Femenino',
+                'slug' => Str::slug('Grupo Femenino')
+            ],
+
+            [
+                'category_id' => 2,
+                'name' => 'Solistas',
+                'slug' => Str::slug('Solistas')
+            ],
+
+            /* Merchandise */
+            [
+                'category_id' => 3,
+                'name' => 'Grupo Masculino',
+                'slug' => Str::slug('Grupo Masculino')
+            ],
+
+            [
+                'category_id' => 3,
+                'name' => 'Grupo Femenino',
+                'slug' => Str::slug('Grupo Femenino')
+            ],
+
+            [
+                'category_id' => 3,
+                'name' => 'Solistas',
+                'slug' => Str::slug('Solistas')
+            ],
+
+        ];
+
+        foreach ($subcategories as $subcategory){
+            Subcategory::factory(1)->create($subcategory);
+        }
     }
 }
