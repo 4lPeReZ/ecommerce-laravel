76b42b1bf3d06ace89f86eec5e9362635b243663
AlvaroPerezRey <aperez.alumno@iesrafaelalberti.es>
2022-05-31 17:39:57 +0200

    Problema de log out arreglado

--- ecommerce/resources/views/livewire/navigation.blade.php
+++ ecommerce/resources/views/livewire/navigation.blade.php
@@ -56,7 +56,8 @@
                                         @csrf
 
                                         <x-jet-dropdown-link href="{{ route('logout') }}"
-                                            @click.prevent="$root.submit();">
+                                        onclick="event.preventDefault();
+                                        this.closest('form').submit();">
                                             {{ __('Log Out') }}
                                         </x-jet-dropdown-link>
                                     </form>
